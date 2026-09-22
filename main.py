eb2aaca31b2a6c85679b7d8777c9f986ea487814
zoumas <zoumas@protonmail.com>
2026-09-23 00:47:22 +0300

    feat(2.3): read book text from file

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def main():
-    print("greetings boots")
+def main() -> None:
+    contents = get_book_text("./books/frankenstein.txt")
+    print(contents)
+
+
+def get_book_text(filepath: str) -> str:
+    with open(filepath, encoding="utf-8") as f:
+        return f.read()
 
 
 if __name__ == "__main__":
