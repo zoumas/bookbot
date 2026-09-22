e5b826f3658ff72c0b68aa44e3846ac525434caf
zoumas <zoumas@protonmail.com>
2026-09-23 01:50:27 +0300

    feat(3.1): sort character counts from most to least common

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from stats import count_characters, count_words
+from stats import count_characters, count_words, sort_by_count
 
 
 def main() -> None:
     text = get_book_text("./books/frankenstein.txt")
     num_words = count_words(text)
     print(f"Found {num_words} total words")
-    char_counts = count_characters(text)
-    print(f"Character count: {char_counts}")
+    char_counts = sort_by_count(count_characters(text))
+    print(char_counts)
 
 
 def get_book_text(filepath: str) -> str:
--- stats.py
+++ stats.py
@@ -1,3 +1,6 @@
+import operator
+
+
 def count_words(text: str) -> int:
     return len(text.split())
 
@@ -7,3 +10,7 @@ def count_characters(text: str) -> dict[str, int]:
     for char in text.lower():
         counts[char] = counts.get(char, 0) + 1
     return counts
+
+
+def sort_by_count(counts: dict[str, int]) -> list[tuple[str, int]]:
+    return sorted(counts.items(), key=operator.itemgetter(1), reverse=True)
