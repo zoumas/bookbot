5037573ce9260420d6df677b04894bd26ecc4c1a
zoumas <zoumas@protonmail.com>
2026-09-23 02:06:12 +0300

    feat(3.2): print formatted report of letter frequencies

--- main.py
+++ main.py
@@ -2,16 +2,35 @@ from stats import count_characters, count_words, sort_by_count
 
 
 def main() -> None:
-    text = get_book_text("./books/frankenstein.txt")
+    filepath = "books/frankenstein.txt"
+    text = get_book_text(filepath)
     num_words = count_words(text)
-    print(f"Found {num_words} total words")
     char_counts = sort_by_count(count_characters(text))
-    print(char_counts)
+    report = format_report(filepath, num_words, char_counts)
+    print(report)
 
 
 def get_book_text(filepath: str) -> str:
     with open(filepath, encoding="utf-8") as f:
         return f.read()
+
+
+def format_report(
+    filepath: str, num_words: int, char_counts: list[tuple[str, int]]
+) -> str:
+    lines = [
+        "============ BOOKBOT ============",
+        f"Analyzing book found at {filepath}...",
+        "----------- Word Count ----------",
+        f"Found {num_words} total words",
+        "--------- Character Count -------",
+    ]
+    for char, count in char_counts:
+        if not char.isalpha():
+            continue
+        lines.append(f"{char}: {count}")
+    lines.append("============= END ===============")
+    return "\n".join(lines)
 
 
 if __name__ == "__main__":
